7ea7668b6cf00688fd7d553fd25e2b88971bc801
Konstantin Lexov <38361424+LexusLight@users.noreply.github.com>
2022-03-19 19:06:03 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,12 +1,13 @@
 import time
 import random
 
-global number
+number = ""
 
 
 # Функция, которая каждые 5 секунд генерит число-хекс в своём потоке
 if __name__ == '__main__':
     while True:
+        global number
         print("NEW NUMBER " + number)
         time.sleep(5.0)
         number = str(random.randint(100000, 999999))
