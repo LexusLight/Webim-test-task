b8b02ea75f3b9d1f584d765b3259b7dd8c8fb87d
Konstantin Lexov <38361424+LexusLight@users.noreply.github.com>
2022-03-19 18:39:17 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,10 +1,11 @@
 import time
 import random
 
-
+print("I AM WORKING2")
 # Функция, которая каждые 5 секунд генерит число-хекс в своём потоке
 if __name__ == '__main__':
     while True:
+        print("I AM WORKING2")
         time.sleep(5.0)
         number = str(random.randint(100000, 999999))
         f = open('text.txt', 'w')
