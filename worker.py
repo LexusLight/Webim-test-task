c343e2a799dc3523113d8e19980376b14281ef2e
Konstantin Lexov <38361424+LexusLight@users.noreply.github.com>
2022-03-19 19:17:49 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,7 +7,8 @@ number = "1"
 # Функция, которая каждые 5 секунд генерит число-хекс в своём потоке
 if __name__ == '__main__':
     while True:
-        print("NEW NUMBER " + number)
-        time.sleep(5.0)
         number = str(random.randint(100000, 999999))
+        os.environ["NUMBER"] = number
+        time.sleep(5.0)
         
+       
