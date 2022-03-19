1b3cba9a03358910dcd3e88e53e63eeaceed56be
Konstantin Lexov <kostyaelizarov@mail.ru>
2022-03-19 16:41:23 +0300

    Update number_worker.py

--- number_worker.py
+++ number_worker.py
@@ -4,10 +4,9 @@ import random
 
 # Функция, которая каждые 5 секунд генерит число-хекс в своём потоке
 from threading import Thread
-while True:
-    global number
-    time.sleep(5.0)
-    number = str(random.randint(100000, 999999))
-    f = open('text.txt', 'w')
-    f.write(number)
-
+if __name__ == '__main__':
+    while True:
+        time.sleep(5.0)
+        number = str(random.randint(100000, 999999))
+        f = open('text.txt', 'w')
+        f.write(number)
