95b97655f5d70dc7ded7b90086c43f1cde8c1a58
Konstantin Lexov <kostyaelizarov@mail.ru>
2022-03-20 14:09:37 +0300

    ф

--- worker.py
+++ worker.py
@@ -5,8 +5,13 @@ import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 cur = conn.cursor()
+
+def connect():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+
 
 def create_table():
     query = "CREATE TABLE numbers (id INTEGER PRIMARY KEY, number VARCHAR(255))"
@@ -30,6 +35,7 @@ number = "1"
 
     # Функция, которая каждые 5 секунд генерит число-хекс в своём потоке
 if __name__ == '__main__':
+    connect()
     create_table()
     while True:
         time.sleep(5.0)
